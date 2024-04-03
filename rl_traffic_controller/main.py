ad557f042b37b3a2b068f6796f1b9cb18bfec587
Abdelaziz W. Farahat <abdelaziz.w.f@gmail.com>
2024-04-03 23:32:55 +0200

    Add logging for main.py

--- rl_traffic_controller/main.py
+++ rl_traffic_controller/main.py
@@ -1,10 +1,13 @@
 import torch
+import logging
 
 from rl_traffic_controller.agent import DQN, Agent
 from rl_traffic_controller.environment import Environment
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+logger = logging.getLogger(__name__)
 
 
 def run(
@@ -20,11 +23,22 @@ def run(
     env = Environment()
     
     if load_nets is True:
-        policy_net.load_state_dict(torch.load("models/policy_net.pt"))
-        target_net.load_state_dict(torch.load("models/target_net.pt"))
+        try:
+            policy_net.load_state_dict(torch.load("models/policy_net.pt"))
+            target_net.load_state_dict(torch.load("models/target_net.pt"))
+            logger.info("Loaded models successfully")
+        except Exception:
+            logger.exception("Failed to load models.")
+            exit()
     else:
         target_net.load_state_dict(policy_net.state_dict())
     
     agent = Agent(policy_net, target_net)
     
-    agent.run(env, num_episodes, checkpoints)
+    logger.info('Started training.')
+    
+    agent.train(env, num_episodes, checkpoints)
+    
+    logger.info('Finished training.')
+    
+    env.destroy()
