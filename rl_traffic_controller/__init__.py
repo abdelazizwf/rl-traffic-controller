0d0e4ffa5a4fd434806e928a77e66c3dded4a53c
Abdelaziz W. Farahat <abdelaziz.w.f@gmail.com>
2024-04-03 22:19:50 +0200

    Add logging for VNC controller

--- rl_traffic_controller/__init__.py
+++ rl_traffic_controller/__init__.py
@@ -1,1 +1,23 @@
+import logging
+import sys
+
 from rl_traffic_controller.main import run
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+fmt = "%(asctime)s %(name)s %(levelname)s: %(message)s"
+datefmt = "%H:%M:%S"
+formatter = logging.Formatter(fmt=fmt, datefmt=datefmt)
+
+file_handler = logging.FileHandler("logs/run.log", mode="w")
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setLevel(logging.INFO)
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
--- rl_traffic_controller/controllers.py
+++ rl_traffic_controller/controllers.py
@@ -1,6 +1,8 @@
 from vncdotool import api
 from PIL import Image
 
+import logging
+
 
 class VNCController:
     """A VNC controller to handle simulation output.
@@ -15,11 +17,15 @@ class VNCController:
     """
     
     def __init__(self, vnc_server, image_path):
+        self._logger = logging.getLogger(f"{__name__}.{self.__class__.__name__}")
+        
         self.vnc_server = vnc_server
         
         try:
             self.client = api.connect(vnc_server, password="abcabc")
+            self._logger.info(f"VNC connection to {vnc_server} established.")
         except Exception:
+            self._logger.exception("VNC connection error.")
             exit()
         
         self.image_path = image_path
@@ -38,10 +44,13 @@ class VNCController:
         """
         try:
             self.client.captureRegion(self.image_path, x, y, w, h)
+            self._logger.debug(f"Captured image at {self.image_path} successfully.")
             return Image.open(self.image_path)
         except Exception:
+            self._logger.exception("Error while capturing image.")
             exit()
     
     def shutdown(self):
         """Disconnect the VNC client."""
         api.shutdown()
+        self._logger.info("VNC connection closed.")
